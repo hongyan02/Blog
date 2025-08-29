b257e3515dfadb4a90701ee5b5b21d0a1145706f
hongyan02 <hongyan.agcl@gmail.com>
2025-08-29 09:42:07 +0800

    add:posts的Metadata自动获取

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -3,13 +3,14 @@ import { AuthProvider } from "@/features/auth/components/AuthContext";
 import { Providers } from "@/shared/providers";
 import AdminBarClient from "@/features/sidebar/ui/AdminBarClient";
 
-export default function GamesLayout({ children }: { children: React.ReactNode }) {
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
     const items: { title: string; slug: string }[] = [
         {
             title: "邀请码",
             slug: "invite",
         },
     ];
+
     return (
         <div className="flex w-full h-full">
             <aside className="w-[20vw] h-full overflow-y-auto p-3 bg-gray-100/70 border-r-2 border-gray-200">
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { requireAuth } from "@/features/auth/auth";
 
 export function middleware(req: NextRequest) {
     const token = req.cookies.get("token")?.value;
